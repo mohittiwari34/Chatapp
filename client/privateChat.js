d331a98eb753e1f9ec8be1c4423515ebddc04854
Mohit Tiwari <mohittiwarigcmb@gmail.com>
2025-11-08 19:39:16 +0530

    video call integrated

--- client/privateChat.js
+++ client/privateChat.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import ReactDOM from "react-dom";
 import { useState } from "react";
 import { Link,useParams } from "react-router";
@@ -10,8 +10,100 @@ import socket from "./socket";
 export default function Pribate(){
     const{userName}=useContext(ChatContext);
     const {id}=useParams();
+    const localVideo=useRef(null);
+    const remoteVideo=useRef(null);
+    const pc=useRef(null);
+    const localStream=useRef(null);
+    const currentOffer=useRef(null);
+    const [icoming,setIncoming]=useState(null);
+    const [inCall,setInCall]=useState(false);
+
+    const iceServers=[
+      {},
+      {
+        urls:"",
+        username:"",
+        credential:" ",
+      },
+    ]
+    const createPc=(targetId)=>{
+      const pc=new RTCPeerConnection({iceServers});
+      pc.onicecandidate=({candidate})=>{
+        if(candidate) socket.emit("candidate",{to:targetId,candidate});
+      };
+      pc.ontrack=(e)=>(remoteVideo.current.srcObject=e.streams[0]);
+      if(localStream.current){
+        localStream.current.getTracks().forEach((t)=>
+        pc.addtTrack(t,localStream.current));
+      }
+      pcRef.current=pc;
+      return pc;
+    };
+
+    const startCamera=async()=>{
+      localStream.current=await navigator.mediaDevices.getUserMedia({
+        video:true,
+        audio:true,
+      })
+      localVideo.current.srcObject=localStream.current;
+    }
+    const startCall=async ()=>{
+      await startCamera();
+      const pc=createPc(id);
+      const offer=await pc.createOffer();
+      await pc.setLocalDescription(offer);
+      socket.emit("call-user",{to: id,fromName:userName});
+      socket.emit("offer",{to:id,offer: pc.localDescription});
+      setInCall(true);
+      
+    }
+    const acceptCall=async ()=>{
+      await startCamera();
+      const pc=createPc(incoming.id);
+      await pc.setRemoteDescription(currentOffer.current);
+      const answer=await pc.createAnswer();
+      await pc.setLocalDescription(answer);
+      socket.emit("answer",{to:incoming.id,answer: pc.localDescription});
+      setInCall(true);
+      setIncoming(null);
+    }
+    const rejectCall=()=>{
+      socket.emit("hangup",{to:incoming.id});
+      setIncoming("null");
+
+    }
+    const hangup=()=>{
+      if(pc.current){
+        pc.current.close();
+
+      }
+      if(localStream.current){
+        localStream.current.getTracks().forEach((t)=>t.stop());
+
+      }
+      remoteVideo.current.srcObject=null;
+      localVideo.current.srcObject=null;
+      socket.emit("hangup",{to:id});
+      setInCall(false);
+    }
+
+  
+
     const [message, setMessage] = useState("");
     const [messages, setMessages] = useState([]);
+    useEffect(()=>{
+      socket.on("incoming-call",({from,fromName})=>{
+        setIncoming({id:from,name:fromName});
+      
+      });
+      socket.on("offer",({from,offer})=>{
+        currentOffer.currrent=offer;
+        setIncoming({id:from});
+      })
+      soc
+
+
+    })
     useEffect(()=>{
         socket.on("recieve",(data)=>{
       setMessages((prev)=>[
