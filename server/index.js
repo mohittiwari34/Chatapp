0703c611fb633712cdf0d560d34bb9e3e8f4e826
Mohit Tiwari <mohittiwarigcmb@gmail.com>
2026-02-07 18:03:56 +0530

    Fix CORS for TURN credentials endpoint

--- server/index.js
+++ server/index.js
@@ -6,6 +6,17 @@ const { Server } = require("socket.io");
 const app = express();
 const server = http.createServer(app);
 
+// Manual CORS Middleware for API endpoints
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 // Allow CORS from frontend
 const io = new Server(server, {
   cors: {
